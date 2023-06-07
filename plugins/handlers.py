3175c426ccd8dae3c92a1f6269c8fa6cb9209bdf
JaloliddinRuzikulov <jaloliddinruzikulov@gmail.com>
2023-06-08 01:43:38 +0500

    add start and stop commands

--- plugins/handlers.py
+++ plugins/handlers.py
@@ -3,6 +3,8 @@ from pyrogram.enums.parse_mode import ParseMode
 from pydub import AudioSegment
 import openai
 
+status = "uxlayabman"
+bot_run = True
 def convert_ogg_to_mp3(ogg_path, mp3_path):
     ogg_audio = AudioSegment.from_ogg(ogg_path)
     ogg_audio.export(mp3_path, format="mp3")
@@ -11,15 +13,26 @@ def convert_ogg_to_mp3(ogg_path, mp3_path):
 @Client.on_message(filters.text & filters.private, group=1)
 async def chatgpt(client, message):
     print(message.text)
-    completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-    {"role": "user", "content": message.text + "\n o'zbek tilida javob ber"}
-    ]
-    )
-    is_unread = await client.read_chat_history(message.chat.id, message.id)
-    print(completion.choices[0].message['content'])
-    await message.reply_text(completion.choices[0].message['content'], quote=True, parse_mode=ParseMode.MARKDOWN)
+    if message.text == "/stop":
+        global bot_run
+        bot_run = False
+        await message.reply("Bot to'xtatildi!")
+        
+    if message.text == "/start":
+        global bot_run
+        bot_run = True
+        await message.reply("Bot to'xtatildi!")
+        
+    if bot_run:
+        completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+        {"role": "user", "content": "O'zbek tilida javob ber.\nMen: Assalomu alaykum men "+status+" biron so'zingiz bo'lsa yozib qoldiring!\nDo'stim: "+message.text+".\nMen: "}
+        ]
+        )
+        is_unread = await client.read_chat_history(message.chat.id, message.id)
+        print(completion.choices[0].message['content'])
+        await message.reply_text(completion.choices[0].message['content'], quote=True, parse_mode=ParseMode.MARKDOWN)
 
 
 
