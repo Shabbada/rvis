614dee860f4ffa1a39ecd346bb420d2efbe5fac0
shabbada <ruzikulov@tuit.uz>
2023-11-08 14:12:38 +0500

    blocker

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ load_dotenv()
 
 openai.api_key = os.getenv('API_KEY')
 api_id = os.getenv('API_ID')
-api_hash =  os.getenv('API_HASH')
+api_hash = os.getenv('API_HASH')
 
 plugins = dict(root="plugins")
 
-Client("my_account", api_id=api_id, api_hash=api_hash, plugins=plugins ).run()
+Client("my_account", api_id=api_id, api_hash=api_hash, plugins=plugins).run()
--- plugins/handlers.py
+++ plugins/handlers.py
@@ -1,58 +1,25 @@
 from pyrogram import Client, filters
 from pyrogram.enums.parse_mode import ParseMode
-from pydub import AudioSegment
 import datetime, logging
 
-import openai
+block_chat = ['-1001652859680']
 
-logging.basicConfig(filename='rvis.log', level=logging.INFO)
-status = "uxlayabman"
-bot_run = True
-def convert_ogg_to_mp3(ogg_path, mp3_path):
-    ogg_audio = AudioSegment.from_ogg(ogg_path)
-    ogg_audio.export(mp3_path, format="mp3")
+logger = logging.basicConfig(filename='rvis_secret_chat.log', level=logging.WARNING)
 
 
-@Client.on_message(filters.text & filters.private, group=1)
-async def chatgpt(client, message):
-    print(message.text)
-    global bot_run
+@Client.on_message(filters.text & filters.group, group=0)
+async def printer(client, message):
+    chatid = message.chat.id
+    if chatid in block_chat:
+        await client.leave_chat(chatid)
+    
+@Client.on_message(filters.private)
+async def secret_chat_logger(client, message):
     user_id = message.from_user.id
-    my_id = 5236426935
-    if message.text == "/stop" and user_id == my_id:
-        bot_run = False
-        await message.reply("Bot to'xtatildi!")
-        current_time = datetime.datetime.now().strftime('%H:%M:%S %d-%m-%Y')
-        logging.info(f'{current_time} : Bot to\'xtatildi!')
-
-    if message.text == "/start" and user_id == my_id:
-        bot_run = True
-        await message.reply("Bot ishga tushdi!")
-        current_time = datetime.datetime.now().strftime('%H:%M:%S %d-%m-%Y')
-        logging.info(f'{current_time} : Bot ishga tushdi!')
-        
-    if bot_run and user_id != my_id:
-        completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-        {"role": "user", "content": "O'zbek tilida javob ber.\nMen: Assalomu alaykum men "+status+" biron so'zingiz bo'lsa yozib qoldiring!\nDo'stim: "+message.text+".\nMen: "}
-        ]
-        )
-        is_unread = await client.read_chat_history(message.chat.id, message.id)
-        print(completion.choices[0].message['content'])
-        await message.reply_text(completion.choices[0].message['content'], quote=True, parse_mode=ParseMode.MARKDOWN)
-        current_time = datetime.datetime.now().strftime('%H:%M:%S %d-%m-%Y')
-        logging.info(f'{current_time} :' + message.text + ' \n: ' +  completion.choices[0].message['content'])
-
-
-
-@Client.on_message(filters.voice & filters.private, group=1)
-async def voice_func(client, message):
-    if message.voice:
-        voice_path = await client.download_media(message.voice)
-        mp3_path = voice_path.replace(".ogg", ".mp3")
-        convert_ogg_to_mp3(voice_path, mp3_path)
-        print("Converted to MP3:", mp3_path)
-        audio_file= open(mp3_path, "rb")
-        transcript = openai.Audio.transcribe("whisper-1", audio_file)
-        await message.reply(transcript['text'])
+    message_text = message.text 
+   
+    current_time = message.date.strftime('%H:%M:%S %d-%m-%Y')
+    if message_text is None:
+        message_text = message
+    log_message = f'{current_time} - User {user_id} {message.from_user.first_name} {message.from_user.last_name}: """{message_text}""" - phone number { message.from_user.phone_number} - username {message.from_user.username}'
+    logging.warn(log_message)  
